8b843afa248ce30655ed0a6fe4f2d9eb24fe118f
Adityanarayan Tiwari <aditya17121997@gmail.com>
2020-07-25 12:43:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+$(document).ready(function(){
+    $('.card-wrapper').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow:$('.next'),
+    prevArrow:$('.prev'),
+    responsive: [
+    {
+        breakpoint: 1240,
+        settings: {
+            arrows: true,
+            slidesToShow: 3
+        }
+        },
+        {
+        breakpoint: 768,
+        settings: {
+            arrows: false,
+            slidesToShow: 3
+        }
+        },
+        {
+        breakpoint: 480,
+        settings: {
+            arrows: false,
+            slidesToShow: 1
+        }
+        }
+    ]
+    });
+});
